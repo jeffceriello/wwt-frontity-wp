64f0215b45aa98c53d7b73fae6253a99125ce68a
EvilDonkeyLondon <info@snipandcode.com>
2021-04-02 21:05:58 +0200

    commit

--- wp-content/themes/wwt/inc/extra.php
+++ wp-content/themes/wwt/inc/extra.php
@@ -15,10 +15,7 @@ if( function_exists('acf_add_options_page') ) {
 
 
 // Google Maps API key
-/*function my_acf_init() {	
-	acf_update_setting('google_api_key', 'AIzaSyBav7E3czLPLufdZGbBUSsdeW02jwu-Ma4');
-}
-add_action('acf/init', 'my_acf_init');*/
+
 
 
 // excerpt length
